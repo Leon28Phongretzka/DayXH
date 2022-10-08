30affd68a2cacb4e4a055b9ca372a49265883248
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2022-10-08 09:58:15 +0700

    08 10 22

---
+++ IDTAPCON001.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0)
+typedef long long ll;
+typedef unsigned long long ul;
+typedef long double ld;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<bool, ll> pbl;
+typedef pair<ul, int> pui;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<int> vi;
+typedef vector<int>::iterator ip;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<pll> vpll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pbl> vpbl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<pdd> vpdd;
+typedef vector<string> vs;
+typedef vector<pui> vpui;
+typedef vector<ul> vu;
+typedef vector<string> vs;
+#define mp make_pair
+#define gl getline
+#define pb push_back
+#define eb emplace_back
+#define ob pop_back
+#define sz(s) ((int)(s.size()))
+#define UM uno\nrdered_map
+#define US uno\nrdered_set
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define fora(i, n) for(auto i:n)
+#define Len 1000005
+const double pi=3.14159265358979323846;
+const int MOD = 1000000007;
+ll tt;
+void substring(string s)
+{
+    ll ans=0;
+    map<string, int> m;
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+        for (int len = 1; len <= n - i; len++)
+        {
+            m[s.substr(i, len)]++;
+        }
+    }
+    for (auto i : m) ans++;
+    cout << ans << " ";
+}
+void solve() 
+{
+    int n; cin >> n;
+    vl a(n);
+    string s;
+    forn(i,n) 
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    forn(i,n) 
+    {
+        s += to_string(a[i]);
+    }
+    // cout << s << endl;
+    substring(s);
+    cout << "[";
+    for(int i=0; i<n; i++)
+    {
+        cout << a[i];
+        if(i!=n-1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+int main() 
+{
+    FAST_IO;
+    // freopen("time.in", "r", stdin); freopen("time.out", "w", stdout);
+    cin >> tt; while(tt--) {solve();}
+    // solve();
+}
