92198b79abf5de6675c28a38004fccabbb285504
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2022-12-23 10:25:23 +0700

    22 12 22

--- Bignum 2.cpp
+++ Bignum 2.cpp
@@ -36,168 +36,82 @@ typedef vector<string> vs;
 #define eb emplace_back
 #define ob pop_back
 #define sz(s) ((int)(s.size()))
-#define UM uno\nrdered_map
-#define US uno\nrdered_set
-#define forn(i, n) for (int i = 1; i <= ll(n); i++)
+#define forn(i,a,b) for (ll i = a; i < b; i++)
+#define forr(i,a,b) for (ll i = a; i >= b; i--)
 #define fora(i, n) for(auto i:n)
 #define Len 100005
+const double pi=3.14159265358979323846;
+const int MOD = 1000000007;
+#define Len 100005
 const int N = 1000007;
-void makeEqualAtFront(vector<int>& A, vector<int>& B)
+// We divine the string to 2 parts: Interger part and Decimal part
+string DivineInterger(string a)
 {
-    int n = A.size();
-    int m = B.size();
-    int diff = abs(n - m);
-    if (n < m) 
+    string res = "";
+    for(int i = 0; i < a.size(); i++)
     {
-        for (int i = 0; i < diff; i++) 
-        {
-            A.insert(A.begin(), 0);
-        }
-    }
-    else 
-    {
-        for (int i = 0; i < diff; i++) 
-        {
-            B.insert(B.begin(), 0);
-        }
-    }
-}
-   
-// Function to make Integral part
-// with equal digits
-void makeEqualAtback(vector<int>& A, vector<int>& B)
-{
-    int n = A.size();
-    int m = B.size();
-    int diff = abs(n - m);
-    if (n < m) 
-    {
-        for (int i = 0; i < diff; i++) 
-        {
-            A.push_back(0);
-        }
-    }
-    else {
-        for (int i = 0; i < diff; i++) 
-        {
-            B.push_back(0);
-        }
-    }
-}
-   
-// Function to add the given large
-// floating point number string
-void findSum(string s1, string s2)
-{
-    int i;
-    vector<int> Ints1, Ints2;
-    vector<int> Fracs1, Fracs2;
-
-    for (i = s1.length() - 1; i > -1; i--) 
-    {
-        // If decimal occurs break
-        if (s1[i] == '.') 
-        {
-            break;
-        }
-        Fracs1.push_back(s1[i] - '0');
-    }
-    i--;
-
-    for (; i > -1; i--) 
-    {
-        Ints1.push_back(s1[i] - '0');
-    }
-
-    for (i = s2.length() - 1; i > -1; i--) 
-    {
-   
-        // If decimal occurs break
-        if (s2[i] == '.') {
-            break;
-        }
-        Fracs2.push_back(s2[i] - '0');
-    }
-    i--;
-    for (; i > -1; i--) {
-        Ints2.push_back(s2[i] - '0');
-    }
-   
-    makeEqualAtFront(Fracs1, Fracs2);
-    makeEqualAtback(Ints1, Ints2);
-    
-    int n = Fracs1.size();
-    int m = Fracs2.size();
-    i = 0;
-    int carry = 0;
-   
-    while (i < n && i < m) 
-    {
-   
-        // Traverse the Fracs1[] and
-        // Fracs2[] and add the digit
-        // and store the carry
-        int sum = Fracs1[i]
-                  + Fracs2[i]
-                  + carry;
-   
-        Fracs1[i] = sum % 10;
-        carry = sum / 10;
-        i++;
-    }
-   
-    int N = Ints1.size();
-    int M = Ints2.size();
-    i = 0;
-   
-    // Adding integer part of
-    // s1 and s2
-    while (i < N && i < M) {
-        int sum = Ints1[i]
-                  + Ints2[i]
-                  + carry;
-        Ints1[i] = sum % 10;
-        carry = sum / 10;
-        i++;
-    }
-    if (carry != 0)
-        Ints1.push_back(carry);
-   
-    // Print the result by appending
-    // Integer and decimal part stored
-    // in Ints1[] and Fracs1[]
-    for (int i = Ints1.size() - 1; i > -1; i--) {
-        cout << Ints1[i];
-    }
-    cout << '.';
-    for (int i = Fracs1.size() - 1; i > -1; i--) {
-        cout << Fracs1[i];
-    }
-}
-string mult(string a, string b)
-{
-    string res="";
-    int n=a.size();
-    int m=b.size();
-    //create res is the answer
-    res.assign(n+m,'0');
-    for(int i=n-1; i>=0;i--)
-    {
-            for(int j=m-1; j>=0; j--)
-            {
-                int mul=(a[i]-'0')*(b[j]-'0');
-                int p1=i+j, p2=i+j+1;
-                int sum=mul+res[p2]-'0';
-                res[p2]=sum%10+'0';
-                res[p1]+=sum/10;
-            }
-    }
-    while (res[0] == '0' && res.length() > 1)
-    {
-            res.erase(0, 1);
+        if(a[i] == '.') break;
+        res += a[i];
     }
     return res;
-}   
+}
+string DivineDecimal(string a)
+{
+    string res = "";
+    for(int i = 0; i < a.size(); i++)
+    {
+        if(a[i] == '.') 
+        {
+            for(int j = i+1; j < a.size(); j++)
+            {
+                res += a[j];
+            }
+            break;
+        }
+    }
+    return res;
+}
+void LengthEqualDecimal(string &a, string &b)
+{
+    while(a.size() < b.size()) a = a + '0';
+    while(b.size() < a.size()) b = b + '0';
+}
+void LengthEqualInterger(string &a, string &b)
+{
+    while(a.size() < b.size()) a = '0' + a;
+    while(b.size() < a.size()) b = '0' + b;
+}
+void findSum(string a, string b)
+{
+    string IntergerA = DivineInterger(a);
+    string IntergerB = DivineInterger(b);
+    string DecimalA = DivineDecimal(a);
+    string DecimalB = DivineDecimal(b);
+    LengthEqualDecimal(DecimalA, DecimalB);
+    LengthEqualInterger(IntergerA, IntergerB);
+    string res = "";
+    int nho = 0;
+    for(int i = DecimalA.size()-1; i >= 0; i--)
+    {
+        int temp = DecimalA[i] - '0' + DecimalB[i] - '0' + nho;
+        nho = temp / 10;
+        temp = temp % 10;
+        res = char(temp + '0') + res;
+    }
+    if(nho > 0) res = char(nho + '0') + res;
+    // if find '.' in string a or b, we add '.' to res
+    if(a.find('.') != string::npos || b.find('.') != string::npos) res = '.' + res;
+    nho = 0;
+    for(int i = IntergerA.size()-1; i >= 0; i--)
+    {
+        int temp = IntergerA[i] - '0' + IntergerB[i] - '0' + nho;
+        nho = temp / 10;
+        temp = temp % 10;
+        res = char(temp + '0') + res;
+    }
+    if(nho > 0) res = char(nho + '0') + res;
+    cout << res;
+}
 // Driver Code
 int main()
 {
